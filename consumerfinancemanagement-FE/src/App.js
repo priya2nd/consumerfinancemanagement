440e80a96c4cc2df5603c903fbf6b545bcaf4d31
priya2nd <priyapspps@gmail.com>
2022-11-09 14:25:21 +0530

    adding productlist and card dashboard FE

--- consumerfinancemanagement-FE/src/App.js
+++ consumerfinancemanagement-FE/src/App.js
@@ -6,7 +6,9 @@ import Home from './pages/Home';
 import Login from './pages/UserLogin';
 import Form from './pages/RegisterUser';
 import UserList from './components/Navbar/UserList';
-import Products from './pages/productlist';
+import Products from './pages/productlist-1st';
+import CardDashboard from './pages/CardDashboard';
+import ProductList from './pages/ProductList';
 
 function App() {
 return (
@@ -20,7 +22,12 @@ return (
 		<Route path='/login' element={<Login />} />
 		<Route path='/sign-up' element={<Form />} />
 		<Route path='/users' element={<UserList />} />
-		<Route path='/users/productlist' element={<Products />} />
+		<Route path='/card-dashboard' element={<CardDashboard />} />
+		<Route path='/product-list' element={<ProductList />} />
+
+
+		{/* //check// */}
+		<Route path='/product-list-1' element={<Products />} />
 	</Routes>
 	</Router>
 );
--- consumerfinancemanagement-FE/src/service/AuthenticationService.js
+++ consumerfinancemanagement-FE/src/service/AuthenticationService.js
@@ -1,6 +1,6 @@
 import axios from 'axios'; // For API calls, we will be using Axios - npm install axios
 
-const API_URL='http://localhost:8080/consumerfinancemanagement/api/';
+const API_URL='http://localhost:8080/consumerfinancemanagement/api/user/';
 const API_URL1='http://localhost:8085/ims/api/dealers';
 
 export const USER_NAME_SESSION_ATTRIBUTE_NAME = 'authenticatedUser';
